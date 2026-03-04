6d6a064c60f052a71dbe88fccc15844b9d8fd341
Alakar7 <114598401+Alakar7@users.noreply.github.com>
2026-03-04 16:03:59 +0530

    Change app.listen to bind to 0.0.0.0

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ app.get("/", (req, res) => {
 
 // ✅ Use Render PORT
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
